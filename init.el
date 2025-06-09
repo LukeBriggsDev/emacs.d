48e4691d2e2da08d10315dde54ae629777f1881b
Luke Briggs <22104392+LukeBriggsDev@users.noreply.github.com>
2025-06-09 22:54:42 +0100

    Remove odorless, add eldoc-box and time-based themes

--- init.el
+++ init.el
@@ -1,12 +1,15 @@
 (org-babel-load-file
  (expand-file-name "config.org"
                    user-emacs-directory))
-(add-hook 'after-save-hook 'my-tangle-config-org-hook-func)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("00d7122017db83578ef6fba39c131efdcb59910f0fac0defbe726da8072a0729"
+	 "59c36051a521e3ea68dc530ded1c7be169cd19e8873b7994bfc02a216041bf3b"
+	 default))
  '(package-selected-packages '(spacious-padding)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
