a59f5a883babfac0d0abbc589d198ddf133970fc
Luke Briggs <22104392+LukeBriggsDev@users.noreply.github.com>
2025-08-02 12:12:31 +0100

    Switch packages to use static (M)ELPA versions

---
+++ packages/apheleia/apheleia-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "apheleia" "20250724.1714"
+  "Reformat buffer stably."
+  '((emacs "27"))
+  :url "https://github.com/radian-software/apheleia"
+  :commit "7dd0c477231165535beba267ff734b6918385cb6"
+  :revdesc "7dd0c4772311"
+  :keywords '("tools")
+  :authors '(("Radian LLC" . "contact+apheleia@radian.codes"))
+  :maintainers '(("Radian LLC" . "contact+apheleia@radian.codes")))
---
+++ packages/emacs-pet/pet-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "pet" "20250801.2334"
+  "Executable and virtualenv tracker for python-mode."
+  '((emacs "26.1")
+    (f     "0.6.0")
+    (map   "3.3.1")
+    (seq   "2.24"))
+  :url "https://github.com/wyuenho/emacs-pet/"
+  :commit "d96aa5fa5c4b0d0070a2dc792b3fe1ec05a2ff93"
+  :revdesc "d96aa5fa5c4b"
+  :keywords '("tools")
+  :authors '(("Jimmy Yuen Ho Wong" . "wyuenho@gmail.com"))
+  :maintainers '(("Jimmy Yuen Ho Wong" . "wyuenho@gmail.com")))
